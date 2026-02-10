dcaf45a01339c2e5785e1dc0e86a38ee3e81a1ed
Rutuja Pathak <connect.pathakrutuj@gmail.com>
2026-02-10 15:17:48 +0530

    solved error

--- react-landing-page-responsive/src/App.tsx
+++ react-landing-page-responsive/src/App.tsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing/Landing";
 import SignIn from "./pages/auth/SignIn";
 import Signup from "./pages/auth/Signup"; // ✅ add this
-import Dashboard from "./pages/dashboard/Dashboard";
+import Dashboard from "./pages/user_dashboard/Dashboard";
+import CompanyDashboard from "./pages/company_dashboard/CompanyDashboard";
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<Signup />} /> {/* ✅ enable */}
       <Route path="/dashboard" element={<Dashboard />} />
+      {/* Company dashboard */}
+      <Route path="/company/dashboard" element={<CompanyDashboard />} />
 
     </Routes>
   );
